00c3b61917fc335b09a5135ed075b3023397dfb3
Tim Broughton <TimBroughton@quickenloans.com>
2018-10-12 15:54:11 -0400

    initil commit

--- LikeToBuyAVowel (tim broughton)/Program.cs
+++ LikeToBuyAVowel (tim broughton)/Program.cs
@@ -10,6 +10,26 @@ namespace LikeToBuyAVowel__tim_broughton_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a line of text to count the vowels in it! :");
+
+            string userInput = Console.ReadLine();
+
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+            int counter = 0;
+
+            foreach(char letter in userInput)
+            {
+                if (vowels.Contains(letter))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("There are {0} vowels in the string {1}",counter, userInput);
+
+            Console.ReadKey();
+
         }
     }
 }
