e8cb6241b0c3d9df2c034fcc54e45eba73408046
Tim Broughton <TimBroughton@quickenloans.com>
2018-10-17 16:44:21 -0400

    works with upper and lowercase now, and option for y

--- LikeToBuyAVowel (tim broughton)/Program.cs
+++ LikeToBuyAVowel (tim broughton)/Program.cs
@@ -10,15 +10,36 @@ namespace LikeToBuyAVowel__tim_broughton_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a line of text to count the vowels in it! :");
+            Console.WriteLine("Before we begin...\nWould you like to consider 'Y' a vowel?\n(type 'y' for yes, anything else for no!)");
+
+            char isY = Console.ReadKey(true).KeyChar;
+
+            if (isY == 'y' || isY == 'Y')
+            {
+                Console.WriteLine("Y will be treated as a vowel!");
+            }
+            else
+            {
+                Console.WriteLine("Y will be treated as a consonant!");
+            }
+
+            Console.WriteLine("\nPlease enter a line of text to count the vowels in it! :");
 
             string userInput = Console.ReadLine();
+            char[] vowels = new char[6];
 
-            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            if (isY == 'y' || isY == 'Y')
+            {
+                vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            }
+            else
+            {
+                vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            }
 
             int counter = 0;
 
-            foreach(char letter in userInput)
+            foreach(char letter in userInput.ToLower())
             {
                 if (vowels.Contains(letter))
                 {
